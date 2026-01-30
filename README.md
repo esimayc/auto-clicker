94911464d9f3fd8da2746c560b5f0115c2575ebb
esimayc <esimaycoskun@gmail.com>
2026-01-30 22:14:36 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Random Delay Auto Clicker (Python)
+
+A simple Python auto-clicker with **randomized delays** to simulate human-like clicking.
+
+## 🚀 Features
+- Start / Stop with **F8**
+- Exit safely with **F9**
+- Random delay between clicks (human-like)
+- Lightweight & easy to use
+
+## 🛠 Requirements
+- Python 3.8+
+- Windows (keyboard library works best on Windows)
+
+## 📦 Installation
+```bash
+pip install -r requirements.txt
+
+## ⚠️ Disclaimer
+
+- This project is for educational purposes only.
+- Use responsibly and according to the rules of the platform you are interacting with.
---
+++ auto_clicker.py
@@ -0,0 +1,59 @@
+import pyautogui
+import keyboard
+import threading
+import time
+import random
+
+
+clicking = False
+exit_program = False
+
+#you can change from here how much time delay do you want
+MIN_DELAY = 0.08
+MAX_DELAY = 0.35
+
+def clicker():
+    """
+    Arka planda çalışan tıklama fonksiyonu
+    """
+    global clicking, exit_program
+
+    while not exit_program: #works till you exit
+        if clicking:
+            pyautogui.click()
+
+            # random wait like a person
+            delay = random.uniform(MIN_DELAY, MAX_DELAY)
+            time.sleep(delay)
+        else:
+            time.sleep(0.1)  
+
+def toggle_clicking():
+    """
+    F8 ile tıklamayı aç/kapat
+    """
+    global clicking
+    clicking = not clicking
+    print("Clicking aktif mi?:", clicking)
+
+def stop_program():
+    """
+    F9 ile programdan tamamen çık
+    """
+    global exit_program
+    exit_program = True
+    print("Program kapatılıyor...")
+
+#you can change from here which key you want
+keyboard.add_hotkey("F8", toggle_clicking)
+keyboard.add_hotkey("F9", stop_program)
+
+# so keyboard keep working
+thread = threading.Thread(target=clicker)
+thread.start()
+
+print("Hazır!")
+print("F8 : Başlat / Durdur")
+print("F9 : Çıkış")
+
+thread.join()
